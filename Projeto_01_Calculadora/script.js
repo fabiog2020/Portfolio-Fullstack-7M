d6a83465122ef56ff885c38da00711f49b3ed91e
fabiog2020 <fabiog2020@gmail.com>
2025-11-02 15:47:58 -0300

    feat: implementacao da funcionalidade de soma da calculadora de dois inputs

--- Projeto_01_Calculadora/script.js
+++ Projeto_01_Calculadora/script.js
@@ -1,3 +1,20 @@
-console.log("Olá, mundo! Meu JS está funcionando.");
+console.log("Calculadora JS iniciada.");
+
+// 1. CONFIGURAÇÃO (Roda uma única vez)
+// Encontra os elementos de que precisaremos.
 let botao = document.getElementById("meuBotao");
-botao.addEventListener('click', function() { alert('Você clicou no botão!'); });
+
+// Encontrando o elemento de exibição
+let areaResultado = document.getElementById("resultado");
+
+// 2. AÇÃO (Roda toda vez que o botão for clicado) 
+botao.addEventListener('click', function() {
+    // 1. CAPTURAR OS NÚMEROS AQUI DENTRO
+    let numero1 = parseInt(document.getElementById("num1").value);
+    let numero2 = parseInt(document.getElementById("num2").value);
+    // 2. CALCULAR
+    let resultado = numero1 + numero2;
+    // 3. EXIBIR
+    let areaResultado = document.getElementById("resultado");
+    areaResultado.innerText = "O resultado é: " + resultado;
+});
