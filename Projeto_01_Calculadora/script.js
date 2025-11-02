70f9fc95f6c77a3a7ce71658daa130ddb4f871fe
fabiog2020 <fabiog2020@gmail.com>
2025-11-02 11:14:13 -0300

    feat: setup inicial do Projeto 1 - Calculadora (HTML, CSS, JS)

---
+++ Projeto_01_Calculadora/script.js
@@ -0,0 +1,3 @@
+console.log("Olá, mundo! Meu JS está funcionando.");
+let botao = document.getElementById("meuBotao");
+botao.addEventListener('click', function() { alert('Você clicou no botão!'); });
